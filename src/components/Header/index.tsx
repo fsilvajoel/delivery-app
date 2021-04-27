43274292872e131bd52646ba4f262fe81c5de1e6
Joel Ferreira da Silva <jfsilva@inf.ufsm.br>
2021-04-27 17:54:16 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+export function Header(){
+  return(
+    <header>
+      teste de header
+      <span>qui, 22 04</span>
+    </header>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import Head from 'next/head'
+import { Header } from '../components/Header'
+export default function Home() {
+  return (
+    <Header/>
+  //  <h1>hellsdasdo</h1>
+  )
+}
