f251faac2f50d5f2222ed502a9edfe11c2c8434e
JOEL FERREIRA DA SILVA <joel.silva@afya.com.br>
2021-07-12 02:05:43 -0300

    update page index

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,61 +1,9 @@
 import Head from 'next/head';
 
 import TabCategories from './components/Tab';
+import BannerIntro from './Delivery/BannerIntro';
 import styles from './Home.module.scss';
-
-const server = {
-  name: 'Comida',
-  image: 'https://i.picsum.photos/id/259/200/300.jpg',
-  categories: [
-    {
-      image: 'https://i.picsum.photos/id/259/200/300.jpg',
-      name: 'Comidas',
-      slug: 'comidas',
-      products: [
-        {
-          name: 'Temaki Filadelfia',
-          slug: 'temaki-filadelfia',
-          description: 'Temaki com tal tal tal e isso e aquilo',
-          obs: 'não vem com moio shoiu',
-          price: '30.00',
-          image: 'https://i.picsum.photos/id/259/200/300.jpg',
-          flavors: [
-            {
-              name: 'Sem arroz',
-              sizes: [
-                {
-                  size: 'normal',
-                  value: '3.00',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          name: 'Uramaki Pokemon',
-          slug: 'UramakiPokemon',
-          description:
-            'Arroz, alga nori, creeam cheese, gergelim, salmão grelado e doritos®\r\n10 unidades',
-          obs: '10 unidades',
-          price: '18.50',
-          image:
-            'https://delivery.growtechnologies.com.br/media/groups/Uramaki_Pokemon.jpg',
-          flavors: [
-            {
-              name: 'Tradicional',
-              sizes: [
-                {
-                  size: 'Normal',
-                  value: 0,
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-  ],
-};
+import { server } from './mock.js';
 
 export default function Home() {
   return (
@@ -64,11 +12,11 @@ export default function Home() {
         <title>zeferino</title>
       </Head>
       <main className={styles.main}>
-        <h2>IDNEXXX</h2>
         <TabCategories
           food={server?.categories}
           // drink={server?.categories}
         />
+        <BannerIntro />
       </main>
     </div>
   );
