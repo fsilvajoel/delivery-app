d209bc0e6ec467b7399fcee20f601226e5307ed7
JOEL FERREIRA DA SILVA <joel.silva@afya.com.br>
2021-08-11 00:09:51 -0300

    insert react-query in apllication

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,8 +1,15 @@
 import { Router } from 'next/router';
 
 import { useEffect } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import 'fontsource-roboto';
 import '~styles/globals.scss';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+import ContextProviders from 'src/ContextProviders';
+
+const queryClient = new QueryClient();
 
 function App({ Component, pageProps }: { Component: any; pageProps: any }) {
   useEffect(() => {
@@ -15,7 +22,13 @@ function App({ Component, pageProps }: { Component: any; pageProps: any }) {
     });
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {/* <ContextProviders> */}
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Component {...pageProps} />;{/* </ContextProviders> */}
+    </QueryClientProvider>
+  );
 }
 
 export default App;
