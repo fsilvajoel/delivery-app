9a7548adc96785f60a912886297a546e8dd5e1f1
JOEL FERREIRA DA SILVA <joel.silva@afya.com.br>
2021-08-11 00:14:09 -0300

    create hooks to consume queryes of products

---
+++ src/hooks/useProducts.ts
@@ -0,0 +1,26 @@
+import { useQuery } from 'react-query';
+
+import {
+  getAllCategories,
+  getAllProductsData,
+} from '~services/Api/productsApi';
+
+export function useAllProducts() {
+  const { isLoading, error, data } = useQuery('productsData', () =>
+    getAllProductsData()
+  );
+  if (isLoading) return 'Loading...';
+
+  if (error) return console.log('houve um erro', error);
+  return data;
+}
+
+export function useAllCategoryes() {
+  const { isLoading, error, data } = useQuery('categoryesData', () =>
+    getAllCategories()
+  );
+  if (isLoading) return 'loading';
+
+  if (error) return console.log('houve um erro', error);
+  return data;
+}
