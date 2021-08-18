ed2fc2c38e2ae56879104eaff859098ccfa37283
JOEL FERREIRA DA SILVA <jfsilva@inf.ufsm.br>
2021-08-18 01:37:13 -0300

    show products info in card component

--- src/components/CardProduct/CardProduct.tsx
+++ src/components/CardProduct/CardProduct.tsx
@@ -1,30 +1,38 @@
-import { Card, CardContent, CardActions, CardMedia } from '@material-ui/core';
+import {
+  Card,
+  CardContent,
+  CardActions,
+  CardMedia,
+  Button,
+} from '@material-ui/core';
+// import { IProducts } from 'src/types/products';
 
 import classes from './CardProduct.module.scss';
+import noMedia from './nophoto.png';
 
-export default function CardProduct() {
-  const image =
-    'https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY';
-  const name = 'teste';
+const CardProduct: React.FC = (props) => {
+  const { data } = props;
+  console.log(data);
 
   return (
     <Card className={classes.card}>
-      <CardMedia className={classes.cardMedia} image={image} title={name} />
-      <CardContent
-        className={classes.CardContent}
-        style={{ minHeight: '250px', height: '260px' }}
-      >
-        <h2 className={classes.cardTitle}>Lorem ipsum, dolor sit amet</h2>
-        <span className={classes.description}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate
-          voluptatum delectus dolorum commodi{' '}
-        </span>
-        <h3 className={classes.price}>R$: 12123</h3>
-        {/* <h3 className={classes.price}>R$: {price}</h3> */}
+      <CardMedia
+        className={classes.cardMedia}
+        image={noMedia}
+        title={data.title}
+      />
+      <CardContent className={classes.CardContent}>
+        <h2 className={classes.cardTitle}>{data.name}</h2>
+        <p className={classes.description}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis a
+          tempora cumque labore illo illum laborum mollitia vero qui explicabo.
+        </p>
       </CardContent>
       <CardActions style={{ justifyContent: 'center' }}>
+        <Button>Ver detalhes</Button>
         {/* <ModalProduct data={props.data} /> */}
       </CardActions>
     </Card>
   );
-}
+};
+export default CardProduct;
