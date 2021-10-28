4510884f9e79e95eb9d7758426f0f54b6522eb31
fsilvajoel <--global>
2021-10-28 01:41:55 -0300

    create Drawer component

--- src/components/Layout/Drawer/Drawer.tsx
+++ src/components/Layout/Drawer/Drawer.tsx
@@ -1,74 +1,35 @@
-import { CSSProperties, UIEvent, useEffect } from 'react';
+import { useState } from 'react';
+
+import Box from '@material-ui/core/Box';
+import Drawer from '@material-ui/core/Drawer';
 
 import { IDrawerProps } from './types';
 
-import scss from './Drawer.module.scss';
-
-import CloseSVG from './images/close.svg';
-
-function Drawer(props: IDrawerProps) {
-  const drawerWrapperClass = [scss.drawerWrapper];
-  const drawerContentClass = [scss.drawerContent];
-
-  const onCloseDrawer = () => {
-    if (props.onClose) props.onClose();
-  };
-
-  const onContentScroll = (event: UIEvent<HTMLDivElement>) => {
-    if (props.onScroll) props.onScroll(event);
-  };
-
-  const getStyle = () => {
-    const style: CSSProperties = {};
-
-    if (props.theme === 'white') {
-      style.filter = 'invert(1)';
-    }
-
-    return style;
-  };
-
-  useEffect(() => {
-    document.body.style.overflow = '';
-
-    if (props.open) {
-      document.body.style.overflow = 'hidden';
-    }
-  }, [props.open]);
-
-  if (props.open) {
-    drawerWrapperClass.push(scss.drawerOpen);
-    drawerContentClass.push(scss.drawerAnimation);
-  }
-
-  if (props.theme) drawerContentClass.push(scss[props.theme]);
-  if (props.fullScreen) drawerContentClass.push(scss.fullScreen);
-  if (props.direction) drawerContentClass.push(scss[props.direction]);
-  if (props.className) drawerContentClass.push(props.className);
+const DrawerSection = (props: IDrawerProps) => {
+  const [state, setState] = useState(false);
 
   return (
-    <div className={drawerWrapperClass.join(' ')}>
-      <div className={scss.drawerContainer} onClick={onCloseDrawer} aria-hidden="true" />
-      <div
-        className={drawerContentClass.join(' ')}
-        onClick={(e) => {
-          e.stopPropagation();
-        }}
-        onScroll={onContentScroll}
-        aria-hidden="true"
+    <>
+      <button type="button" onClick={() => setState(true)}>
+        {props.buttonContent}
+      </button>
+      <Drawer
+        anchor={props.direction}
+        open={state}
+        onClose={() => setState(false)}
       >
-        {props.closeButton && (
-          <CloseSVG style={getStyle()} className={scss.close} onClick={onCloseDrawer} />
-        )}
-        {props.children}
-      </div>
-    </div>
+        <Box
+          sx={{ width: 250 }}
+          // sx={{` width: ${props.size}` }}
+          role="presentation"
+          onClick={() => setState(false)}
+          onKeyDown={() => setState(false)}
+        >
+          {props.children}
+        </Box>
+      </Drawer>
+    </>
   );
-}
+};
 
-Drawer.defaultProps = {
-  theme: 'black',
-  direction: 'left',
-} as IDrawerProps;
-
-export default Drawer;
+export default DrawerSection;
--- src/components/Layout/Drawer/types.ts
+++ src/components/Layout/Drawer/types.ts
@@ -1,16 +1,6 @@
-import { UIEvent } from 'react';
-
 export interface IDrawerProps {
   className?: string;
-  theme?: 'black' | 'white';
-  direction?: 'left' | 'right' | 'bottom';
-
-  open: boolean;
-  fullScreen?: boolean;
-  closeButton?: boolean;
-
-  onClose: () => void;
-  onScroll: (event: UIEvent<HTMLDivElement>) => void;
-
+  direction: 'left' | 'right' | 'bottom';
+  buttonContent: string;
   children: React.ReactNode;
 }
